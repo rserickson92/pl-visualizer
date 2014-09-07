1708504c9d5d84a4ab89bf406da189a31c28ddbf
rserickson92 <rserickson92@gmail.com>
2014-09-06 23:51:46 -0700

    refactor code layout and add filtering by paradigm

--- plot.js
+++ plot.js
@@ -1,4 +1,4 @@
-d3.json("data.json", function(err, data) {
+(function() {
   var margin = {top: 20, right: 20, bottom: 20, left: 20},
   padding = {top: 60, right: 60, bottom: 60, left: 60},
   outerWidth = 960, outerHeight = 500,
@@ -6,59 +6,83 @@ d3.json("data.json", function(err, data) {
   innerHeight = outerHeight - margin.top - margin.bottom,
   w = innerWidth - padding.left - padding.right, 
   h = innerHeight - padding.top - padding.bottom,
+  svg = d3.select("body").append("svg")
+    .attr("width", outerWidth)
+    .attr("height", outerHeight)
+    .append("g")
+    .attr("transform", "translate("+margin.left+","+margin.top+")"),
   year = function(d) { return d["year"]; },
   repos = function(d) { return d["nbRepos"]; },
-  xScale = d3.scale.linear()
-                   .domain(d3.extent(data, year))
-		   .range([0, w]),
-  yScale = d3.scale.log()
-                   .domain(d3.extent(data, repos))
-		   .range([h, 0]),
-  xAxis = d3.svg.axis()
-                .scale(xScale)
-		.orient("bottom"),
-  yAxis = d3.svg.axis()
-                .scale(yScale)
-		.orient("left"),
-  svg = d3.select("body").append("svg")
-          .attr("width", outerWidth)
-	  .attr("height", outerHeight)
-          .append("g")
-          .attr("transform", "translate("+margin.left+","+margin.top+")");
+  xScale, yScale;
 
-  svg.selectAll("circle")
-     .data(data)
-     .enter()
-     .append("circle")
-     .attr("cx", function(d) {
-       return xScale(year(d));
-     })
-     .attr("cy", function(d) {
-       return yScale(repos(d));
-     })
-     .attr("r", 5);
-  svg.selectAll("text")
-     .data(data)
-     .enter()
-     .append("text")
-     .text(function(d) {
-       return d["name"];
-     })
-     .attr("x", function(d) {
-       return xScale(year(d));
-     })
-     .attr("y", function(d) {
-       return yScale(repos(d));
-     });
+  d3.json("data.json", function(err, data) {
+    initPlot(data);
+    d3.select("select").on("change", function(){
+      drawPlot(data, this.value);
+    });
+  });
 
-  svg.append("g")
-     .attr("class", "axis")
-     .attr("transform", "translate(0,"+h+")")
-     .call(xAxis);
+  var selectAllAndFilter = function(selector, data, paradigm) {
+    var key = function(d) { return d.name };
+    if(paradigm != "all") {
+      return svg.selectAll(selector).data(data.filter(function(d) {
+	return _.contains(d["paradigms"], paradigm);
+      }), key);
+    }
+    return svg.selectAll(selector).data(data, key);
+  }
+  
+  var drawPlot = function(data, paradigm) {
+    var circleData = selectAllAndFilter("circle", data, paradigm);
+    circleData.enter()
+      .append("circle")
+      .attr("cx", function(d) {
+	return xScale(year(d));
+      })
+      .attr("cy", function(d) {
+	return yScale(repos(d));
+      })
+      .attr("r", 5);
+    circleData.exit().remove();
+    var textData = selectAllAndFilter("text", data, paradigm);
+    textData.enter()
+      .append("text")
+      .text(function(d) {
+	return d["name"];
+      })
+      .attr("x", function(d) {
+	return xScale(year(d));
+      })
+      .attr("y", function(d) {
+	return yScale(repos(d));
+      });
+    textData.exit().remove();
+  };
 
-  svg.append("g")
-     .attr("class", "axis")
-     .call(yAxis);
+  var initPlot = function(data) {
+    xScale = d3.scale.linear()
+      .domain(d3.extent(data, year))
+      .range([0, w]),
+    yScale = d3.scale.log()
+      .domain(d3.extent(data, repos))
+      .range([h, 0]);
+    var xAxis = d3.svg.axis()
+      .scale(xScale)
+      .orient("bottom"),
+    yAxis = d3.svg.axis()
+      .scale(yScale)
+      .orient("left");
 
+    svg.append("g")
+      .attr("class", "axis")
+      .attr("transform", "translate(0,"+h+")")
+      .call(xAxis);
 
-});
+    svg.append("g")
+      .attr("class", "axis")
+      .call(yAxis);
+    
+    drawPlot(data, "all");
+
+  };
+})()
